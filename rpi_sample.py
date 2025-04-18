5cfc83744c6a7a4309bef02f2fcfa747d0242cf8
Mark Kempin <mark@kempin.co>
2025-04-18 18:13:45 +0800

    very simple

--- rpi_sample.py
+++ rpi_sample.py
@@ -1,18 +1,2 @@
-import RPi.GPIO as GPIO
-import time
-
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-
-# Blink LED
-try:
-    while True:
-        GPIO.output(17, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(17, GPIO.LOW)
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+# Simple Python program
+print("Hello, World!")
